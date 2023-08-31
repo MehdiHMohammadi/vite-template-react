9f3495d110800c92c0110664485d114d0804e866
MehdiHMohammadi <MehdihMohammadi@gmail.com>
2023-08-31 15:49:04 +0330

    Remove With Inline Hamdler

--- src/components/listItem.jsx
+++ src/components/listItem.jsx
@@ -8,9 +8,6 @@ const ListItem = ({ id, url, title, author, points, onRemoveItem }) => {
     points: PropTypes.number,
     onRemoveItem: PropTypes.func,
   };
-  const handleRemoveItem = () => {
-    onRemoveItem(id);
-  };
 
   console.log("List Items");
   return (
@@ -31,7 +28,7 @@ const ListItem = ({ id, url, title, author, points, onRemoveItem }) => {
 
         <div className="flex">
           <button
-            onClick={handleRemoveItem}
+            onClick={() => onRemoveItem(id)}
             // type="submit"
             className="ml-8 text-white  right-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800"
           >
